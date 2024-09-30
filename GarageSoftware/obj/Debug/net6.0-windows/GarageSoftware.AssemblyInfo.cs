80fddb699dce371bd9c5b45a0f35bc930ab1dcd2
syvano <longmarchmotors@gmail.com>
2024-09-30 13:16:08 +0100

    changed config file

--- GarageSoftware/obj/Debug/net6.0-windows/GarageSoftware.AssemblyInfo.cs
+++ GarageSoftware/obj/Debug/net6.0-windows/GarageSoftware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarageSoftware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eaa0e5697373398e9f14846582c2c823d8492af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621ed03ffb6bf4ca68413420fd6bed3b3e5a1da1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarageSoftware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarageSoftware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
